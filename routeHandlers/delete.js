1d37fbaee18e12c9ffc6ea509f8698381ffd43cc
Nghe-HP <nghia89hoang@gmail.com>
2016-10-24 13:38:50 +0700

    fix bug

--- routeHandlers/delete.js
+++ routeHandlers/delete.js
@@ -11,6 +11,6 @@ module.exports = function deleteHandler(onDelete) {
     rimraf.promise(filePath).then(dummy => {
       onDelete(req.filePath, req.isDir)
     })
-    return Promise.resolve('next')
+    Promise.resolve('next')
   }
 }
